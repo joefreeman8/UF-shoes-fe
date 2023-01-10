45fd66d064a9375e9a83fd921dcd229a947270c7
Joe Freeman <joefreeman8@gmail.com>
2023-01-10 19:33:30 +0000

    reviews displaying on page, need to style

--- src/components/products/ProductShow.js
+++ src/components/products/ProductShow.js
@@ -34,60 +34,45 @@ function ProductShow() {
 
 
   return (
-    <Container maxWidth='lg' sx={{ display: 'flex' }} className='product-show'>
-      <Box sx={{ mt: 5 }}>
-        <img src={singleProduct?.image} alt={singleProduct?.name} />
-      </Box>
-      <Box sx={{ mt: 5 }}>
-        <CardContent>
-          <Typography variant='h3' component='p'>
-            {singleProduct?.name}
-          </Typography>
-          <Typography variant='h4' component='p'>
-            £ {singleProduct?.price}
-          </Typography >
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={goToIndex}>back to shop</Button>
-          {isLoggedIn && (
-            <Link to={`/shop/${singleProduct?._id}/reviews`}>
-              <Button size="small">review</Button>
-            </Link>
-          )}
-          <Button size="small">Add to Cart</Button>
-        </CardActions>
-      </Box>
-    </Container>
-
-
-
-    // <>
-    //   <h1>ProductShow</h1>
-    //   <div>
-    //     {product && (
-    //       <div key={product._id}>
-    //         <h2>{product?.name}</h2>
-    //         <p>{product?.type}</p>
-    //         <img src={product?.image} alt={product.name} />
-    //         <p>£{product?.price}</p>
-    //         <div>
-    //           <h4>Comments</h4>
-    //           {product?.comments.map(comment => (
-    //             <div key={comment._id} >
-    //               <p><strong>{comment.text}</strong></p>
-    //               <p><strong>{comment.rating}</strong></p>
-    //               <p><strong>{comment.addedBy}</strong></p>
-    //               <p><strong>{comment.createdAt}</strong></p>
-    //             </div>
-    //           )
-    //           )}
-    //           <p>{product.comments.text}</p>
-    //           <p>{product.comments.rating}</p>
-    //         </div>
-    //       </div>
-    //     )}
-    //   </div>
-    // </>
+    <>
+      <Container maxWidth='lg' sx={{ display: 'flex' }} className='product-show'>
+        <Box sx={{ mt: 5 }}>
+          <img src={singleProduct?.image} alt={singleProduct?.name} />
+        </Box>
+        <Box sx={{ mt: 5 }}>
+          <CardContent>
+            <Typography variant='h3' component='p'>
+              {singleProduct?.name}
+            </Typography>
+            <Typography variant='h4' component='p'>
+              £ {singleProduct?.price}
+            </Typography >
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={goToIndex}>back to shop</Button>
+            {isLoggedIn && (
+              <Link to={`/shop/${singleProduct?._id}/reviews`}>
+                <Button size="small">review</Button>
+              </Link>
+            )}
+            <Button size="small">Add to Cart</Button>
+          </CardActions>
+        </Box>
+      </Container>
+      {!!singleProduct?.reviews.length && (
+        <Container>
+          <Box>
+            {singleProduct?.reviews.map((review) => (
+              <div key={review._id}>
+                {review.text}
+                {review.rating}
+                {review.addedBy}
+              </div>
+            ))}
+          </Box>
+        </Container>
+      )}
+    </>
   )
 }
 
