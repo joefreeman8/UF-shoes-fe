5bb45c435f9f43031419f0549f721c8546ac495f
Joe Freeman <joefreeman8@gmail.com>
2023-03-01 15:56:54 +0000

    added in a filter dropdown for my products index page and discovered worlds most annoying bug of padding covering my select element and stopping it from being able to be clicked

--- src/components/products/ProductIndex.js
+++ src/components/products/ProductIndex.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Container, Grid } from '@mui/material'
+import { Box, Container, Grid, Select, MenuItem } from '@mui/material'
 
 import ProductCard from './ProductCard'
 import { API } from '../lib/api'
@@ -7,21 +7,42 @@ import '../../styles/ProductIndex.scss'
 
 function ProductIndex() {
   const [products, setProducts] = useState([])
+  const [filteredBrands, setFilteredBrands] = useState([])
+  const [filters, setFilters] = useState({ brand: 'All' })
 
 
   useEffect(() => {
     const getData = async () => {
       try {
         const { data } = await API.GET(API.ENDPOINTS.allProducts)
-        console.log(data)
         setProducts(data)
+
+
       } catch (err) {
         console.log(err)
       }
-
     }
     getData()
-  }, [])
+  }, [filters])
+
+
+  const handleFilterChange = (e) => {
+    setFilters({ ...filters, brand: e.target.value })
+  }
+
+  // this useEffect is waiting an listening for a change in the filtering. 
+  useEffect(() => {
+    if (products.length) {
+      setFilteredBrands(products.filter(product => {
+        return filters.brand === product.brand || filters.brand === 'All'
+      }))
+    }
+  }, [filters, products])
+
+  //removes the duplicates from my brands. 
+  const brands = [...new Set(products?.map(product => product.brand))].sort()
+  brands.unshift('All')
+
 
   return (
     <Container
@@ -29,8 +50,15 @@ function ProductIndex() {
       sx={{ mt: 5 }}
     >
       <h1 className='products-header'>Products</h1>
+      <Box className='filter-container'>
+        <Select id="brand-filter" onChange={handleFilterChange} value={filters.brand}>
+          {brands.map(brand => (
+            <MenuItem key={brand} value={brand}>{brand}</MenuItem>
+          ))}
+        </Select>
+      </Box>
       <Grid container spacing={4}>
-        {products?.map(product => (
+        {(filteredBrands.length ? filteredBrands : products).map(product => (
           <Grid item xs={6} sm={4} md={3} key={product._id}>
             <ProductCard
               name={product.name}
@@ -41,7 +69,7 @@ function ProductIndex() {
           </Grid>
         ))}
       </Grid>
-    </Container>
+    </Container >
   )
 }
 
